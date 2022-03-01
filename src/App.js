b40c7983ab7a2eb72cae3bd3e804424ebe4ca311
mounuikavedhanshi <89015579+mounuikavedhanshi@users.noreply.github.com>
2022-03-01 22:17:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useState} from 'react';
+import { Modal,Button,Form } from 'react-bootstrap';
 function App() {
+  const[data,setData] = useState({
+username:'',
+comment:'',
+rating:'',
+
+
+  });
+  const {username,comment,rating} = data;
+  const onChange=e=>{
+    setData({...data,[e.target.name]:[e.target.value]})
+  }
+  const submitHandler=e=>{
+    e.preventDefault();
+    console.log(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+   <Modal.Dialog>
+  <Modal.Header closeButton>
+    <Modal.Title>Submit Comment</Modal.Title>
+  </Modal.Header>
+
+  <Modal.Body>
+  <Form onSubmit={submitHandler}>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+  <Form.Label >Rating</Form.Label>
+
+  <Form.Select onChange={onChange} value={rating} name="rating"   aria-label="Default select example">
+
+  <option >1</option>
+  <option >2</option>
+  <option >3</option>
+  <option>4</option>
+    <option>5</option>
+
+</Form.Select>
+</Form.Group>
+
+  <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+    <Form.Label>Your Name</Form.Label>
+    <Form.Control type="name" value={username}  name="username" onChange={onChange} placeholder="Your Name" />
+  </Form.Group>
+  <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Comment</Form.Label>
+    <Form.Control as="textarea" name="comment" onChange={onChange} value={comment} rows={5} />
+  </Form.Group>
+  <Button variant="primary" type="submit" name="submit">Submit</Button>
+
+</Form>
+  </Modal.Body>
+    </Modal.Dialog>
     </div>
   );
 }
-
 export default App;
